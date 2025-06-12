599bba5e26fd3b4843aafd097b7380671550b2fa
Ali Azizi <aliazizi.79a@gmail.com>
2025-06-13 02:25:48 +0330

    Create BGPedometerConstants.kt

---
+++ background_pedometer_android/android/src/main/kotlin/com/github/aliazizi/background_pedometer_android/BGPedometerConstants.kt
@@ -0,0 +1,14 @@
+package com.github.aliazizi.background_pedometer_android
+
+object BGPedometerConstants {
+
+    private const val PACKAGE = "com.github.aliazizi.background_pedometer_android"
+
+    const val ACTION_START_PEDOMETER_SERVICE = "$PACKAGE.START"
+    const val ACTION_START_BY_RECEIVERS = "$PACKAGE.START_BY_RECEIVERS"
+    const val ACTION_STOP_PEDOMETER_SERVICE = "$PACKAGE.STOP"
+
+
+    const val EXTRA_BG_SETTINGS = "$PACKAGE.EXTRA_BG_SETTINGS"
+    const val EXTRA_BG_SERVICE_START_ORIGIN = "$PACKAGE.EXTRA_BG_SERVICE_START_ORIGIN"
+}
