908bae6e1b030eb76a5867161807929bc1cc259c
Ali Azizi <aliazizi.79a@gmail.com>
2025-06-13 02:27:42 +0330

    pedometer settings delegation added

--- background_pedometer_android/android/src/main/kotlin/com/github/aliazizi/background_pedometer_android/types/BGPedometerSettings.kt
+++ background_pedometer_android/android/src/main/kotlin/com/github/aliazizi/background_pedometer_android/types/BGPedometerSettings.kt
@@ -1,12 +1,16 @@
 package com.github.aliazizi.background_pedometer_android.types
 
-import android.annotation.SuppressLint
 import android.content.Context
+import android.content.SharedPreferences
 import android.hardware.Sensor
+import android.os.Parcel
 import android.os.Parcelable
+import android.util.Base64
 import com.github.aliazizi.background_pedometer_android.exceptions.MissingNotificationDrawableException
 import kotlinx.parcelize.IgnoredOnParcel
 import kotlinx.parcelize.Parcelize
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
 
 @Parcelize
 data class BGPedometerSettings(
@@ -37,7 +41,9 @@ data class BGPedometerSettings(
                 notification = notification
             )
         }
+
     }
+
 }
 
 @Parcelize
@@ -87,5 +93,74 @@ enum class BGPedometerSensorType(val sensorTypeId: Int) {
         }
 
         infix fun byName(name: String) = nameToTypeMap[name.uppercase()]
+    }
+}
+
+
+class BGPedometerSettingsDelegate(
+    private val prefs: SharedPreferences
+) : ReadWriteProperty<Any?, BGPedometerSettings?> {
+
+    override fun getValue(thisRef: Any?, property: KProperty<*>): BGPedometerSettings? {
+        if (!prefs.contains("autoRestart")) {
+            return null
+        }
+
+        val sensorFallbackOrder = prefs.getString("sensorFallbackOrder", "")?.split(",")
+            ?.mapNotNull { BGPedometerSensorType.byName(it) } ?: return null
+
+        val hasNotification = prefs.getBoolean("notif_enabled", false)
+        val notification = if (hasNotification) {
+            val channelId = prefs.getString("notif_channelId", null) ?: return null
+            val channelName = prefs.getString("notif_channelName", null) ?: return null
+            val icon = prefs.getString("notif_icon", null) ?: return null
+            val messageTemplate = prefs.getString("notif_messageTemplate", null) ?: return null
+            val notificationID = prefs.getInt("notif_notificationID", -1)
+            if (notificationID == -1) return null
+
+            BGPedometerNotificationSettings(
+                channelId = channelId,
+                channelName = channelName,
+                icon = icon,
+                messageTemplate = messageTemplate,
+                notificationID = notificationID,
+                channelDescription = prefs.getString("notif_channelDescription", null)
+            )
+        } else null
+
+        return BGPedometerSettings(
+            autoRestart = prefs.getBoolean("autoRestart", false),
+            autoRunOnBoot = prefs.getBoolean("autoRunOnBoot", false),
+            autoRunOnMyPackageReplaced = prefs.getBoolean("autoRunOnMyPackageReplaced", false),
+            shutdownAware = prefs.getBoolean("shutdownAware", false),
+            sensorFallbackOrder = sensorFallbackOrder,
+            notification = notification
+        )
+    }
+
+    override fun setValue(thisRef: Any?, property: KProperty<*>, value: BGPedometerSettings?) {
+
+        prefs.edit().apply {
+            if (value == null) {
+                clear()
+            } else {
+                putBoolean("autoRestart", value.autoRestart)
+                putBoolean("autoRunOnBoot", value.autoRunOnBoot)
+                putBoolean("autoRunOnMyPackageReplaced", value.autoRunOnMyPackageReplaced)
+                putBoolean("shutdownAware", value.shutdownAware)
+                putString("sensorFallbackOrder", value.sensorFallbackOrder.joinToString(","))
+
+                value.notification?.let {
+                    putString("notif_channelId", it.channelId)
+                    putString("notif_channelName", it.channelName)
+                    putString("notif_icon", it.icon)
+                    putString("notif_messageTemplate", it.messageTemplate)
+                    putInt("notif_notificationID", it.notificationID)
+                    putString("notif_channelDescription", it.channelDescription)
+                    putBoolean("notif_enabled", true)
+                } ?: putBoolean("notif_enabled", false)
+            }
+            apply()
+        }
     }
 }
