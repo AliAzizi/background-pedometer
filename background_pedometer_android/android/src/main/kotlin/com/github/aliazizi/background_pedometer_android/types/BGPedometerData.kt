4989153068dc75a0bb0fe03f9d5f2c172b124a20
Ali Azizi <aliazizi.79a@gmail.com>
2025-06-12 22:09:53 +0330

    Create BGPedometerData.kt

---
+++ background_pedometer_android/android/src/main/kotlin/com/github/aliazizi/background_pedometer_android/types/BGPedometerData.kt
@@ -0,0 +1,19 @@
+package com.github.aliazizi.background_pedometer_android.types
+
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZonedDateTime
+
+class BGPedometerData private constructor(
+    private val numberOfSteps: UInt, private val startUtc: Instant, private val endUtc: Instant
+) {
+    init {
+        require(!startUtc.isAfter(endUtc)) { "Start time must not be after end time" }
+    }
+
+    fun toMapForDart(): Map<String, Any> = mapOf(
+        "numberOfSteps" to numberOfSteps,
+        "start" to startUtc.toEpochMilli(),
+        "end" to endUtc.toEpochMilli()
+    )
+}
