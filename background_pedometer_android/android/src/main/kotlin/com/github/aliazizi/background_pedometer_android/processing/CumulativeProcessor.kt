0b62ee2d1ca1a9e7319f5ca70db9cc06d9af1d7d
Ali Azizi <aliazizi.79a@gmail.com>
2025-06-13 06:04:08 +0330

    Create CumulativeProcessor.kt

---
+++ background_pedometer_android/android/src/main/kotlin/com/github/aliazizi/background_pedometer_android/processing/CumulativeProcessor.kt
@@ -0,0 +1,11 @@
+package com.github.aliazizi.background_pedometer_android.processing
+
+import android.hardware.SensorEvent
+
+class CumulativeProcessor : BGPedometerSensorProcessor {
+
+    override fun process(event: SensorEvent): Int {
+        TODO("Not yet implemented")
+    }
+
+}
