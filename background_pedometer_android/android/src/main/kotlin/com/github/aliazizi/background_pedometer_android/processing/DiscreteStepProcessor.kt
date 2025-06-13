8749e3d212439a7f64c48f49ace893f862191c28
Ali Azizi <aliazizi.79a@gmail.com>
2025-06-13 06:04:13 +0330

    Create DiscreteStepProcessor.kt

---
+++ background_pedometer_android/android/src/main/kotlin/com/github/aliazizi/background_pedometer_android/processing/DiscreteStepProcessor.kt
@@ -0,0 +1,9 @@
+package com.github.aliazizi.background_pedometer_android.processing
+
+import android.hardware.SensorEvent
+
+class DiscreteStepProcessor: BGPedometerSensorProcessor {
+    override fun process(event: SensorEvent): Int {
+        TODO("Not yet implemented")
+    }
+}
